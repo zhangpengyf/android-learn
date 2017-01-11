f15a784749832d7561dfa615ebcae31e783128f9
zhangpeng <zhangpeng01@51talk.com>
2017-01-11 14:31:09 +0800

    ProgressBar

--- UIWidgetTest/app/src/main/java/com/learn/android/uiwidgettest/MainActivity.java
+++ UIWidgetTest/app/src/main/java/com/learn/android/uiwidgettest/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import static android.R.attr.onClick;
@@ -16,6 +17,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
     private EditText editText;
     private ImageView imageView;
+    private ProgressBar progressBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +26,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
         button.setOnClickListener(this);
         editText = (EditText)findViewById(R.id.edit_text);
         imageView = (ImageView)findViewById(R.id.image_view);
+        progressBar = (ProgressBar)findViewById(R.id.progress_bar);
     }
 
     @Override
@@ -34,6 +37,14 @@ public void onClick(View var1) {
             String inputText = editText.getText().toString();
             Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
             imageView.setImageResource(R.drawable.webrtc);
+            if (progressBar.getVisibility() == View.GONE) {
+                progressBar.setVisibility(View.VISIBLE);
+            } else {
+                progressBar.setVisibility(View.GONE);
+            }
+            int progress = progressBar.getProgress();
+            progress = progress + 10;
+            progressBar.setProgress(progress);
             break;
         default:
             break;
