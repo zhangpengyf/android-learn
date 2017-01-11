1c34212eb46998f80bbba99a61901946b3a3100a
zhangpeng <zhangpeng01@51talk.com>
2017-01-11 14:22:41 +0800

    editview,image view

---
+++ UIWidgetTest/app/src/main/java/com/learn/android/uiwidgettest/MainActivity.java
@@ -0,0 +1,42 @@
+package com.learn.android.uiwidgettest;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import static android.R.attr.onClick;
+
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private EditText editText;
+    private ImageView imageView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Button button = (Button)findViewById(R.id.button);
+        button.setOnClickListener(this);
+        editText = (EditText)findViewById(R.id.edit_text);
+        imageView = (ImageView)findViewById(R.id.image_view);
+    }
+
+    @Override
+public void onClick(View var1) {
+    switch (var1.getId()){
+        case R.id.button:
+            Log.d("MainActivity", "press button");
+            String inputText = editText.getText().toString();
+            Toast.makeText(MainActivity.this, inputText, Toast.LENGTH_SHORT).show();
+            imageView.setImageResource(R.drawable.webrtc);
+            break;
+        default:
+            break;
+    }
+}
+}
